13beb59f6b1f3067cd21f1e4591f41d5ecead81e
Seb Peterson <sebastien.peterson@gmail.com>
2023-02-15 12:12:11 +0100

    init

--- xbrl_utils.py
+++ xbrl_utils.py
@@ -9,6 +9,11 @@ from xbrl.instance import XbrlParser, XbrlInstance
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+#https://py-xbrl.readthedocs.io/en/latest/usage.html#offline
+cache: HttpCache = HttpCache('./cache')
+cache.set_headers({'From': 'seb@greenrock.be', 'User-Agent': 'py-xbrl/2.1.0'})
+parser = XbrlParser(cache)
+
 
 def parse_xbrl(xbrl_file):
     """ Call to NBB API /authentic/xxx/references 
@@ -18,22 +23,12 @@ def parse_xbrl(xbrl_file):
         - parsed dict
     """
 
-    cache: HttpCache = HttpCache('./cache')
-    cache.set_headers({'From': 'seb@greenrock.be', 'User-Agent': 'py-xbrl/2.1.0'})
-    parser = XbrlParser(cache)
+    # Online
+    #schema_url = "https://www.nbb.be/doc/ba/xbrl/Taxo2019bis/files/pfs-full-2020-02-01.xsd"
+    #inst: XbrlInstance = parser.parse_instance(schema_url)
 
-    schema_url = "https://www.nbb.be/doc/ba/xbrl/Taxo2019bis/files/pfs-full-2020-02-01.xsd"
-    inst: XbrlInstance = parser.parse_instance(schema_url)
-
-    # # Parse file
-    # xbrl_parser = XBRLParser()
-    # xbrl = xbrl_parser.parse(open(xbrl_file))
-
-
-    # # Serialize object
-    # gaap_obj = xbrl_parser.parseGAAP(xbrl, doc_date="20221231", context="current", ignore_errors=0)
-
-    # serializer = GAAPSerializer()
-    # result = serializer.dump(gaap_obj)
+    # Offline
+    schema_path = "./taxonomy/be_fr_pfs_ci_2020_02_01/pfs-2020-02-01-reference.xml"
+    inst: XbrlInstance = parser.parse_instance(schema_path)
 
     logger.debug(f'inst: {inst}')
